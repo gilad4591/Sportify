d6f1f488aae85ebac7a6d36a74854c013dc703a8
tamiryak <45327624+tamiryak@users.noreply.github.com>
2020-04-05 19:14:36 +0300

    fw

---
+++ app/src/main/java/com/example/team24p/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.team24p;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent stNav = new Intent(getApplicationContext(),MapsActivity.class);
+                startActivity(stNav);
+            }
+        });
+    }
+}
