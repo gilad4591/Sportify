80bf8b89101abd217d2078b2f80f01285426826f
tamiryak <45327624+tamiryak@users.noreply.github.com>
2020-04-06 15:33:12 +0300

    location location location

--- app/src/main/java/com/example/team24p/MapsActivity.java
+++ app/src/main/java/com/example/team24p/MapsActivity.java
@@ -8,6 +8,9 @@ import android.location.LocationListener;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -15,44 +18,97 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.Map;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener,GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
     private FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
-    private DatabaseReference mRf = mDatabase.getReference();
-    private DatabaseReference mchild = mRf.child("0");
-
+    private DatabaseReference mRef = mDatabase.getReference();
+    private FusedLocationProviderClient fusedLocationClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        // Retrieve the content view that renders the map.
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        DatabaseReference rr = mchild.child("Activity");
-        rr.
 
-        }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
     }
 
     @Override
+    public void onMapReady(final GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location
+
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //private Fused
+                Double lat=0.0;
+                Double lon=0.0;
+//                Object a = dataSnapshot.getValue();
+                ArrayList<Map<String, String>> locations = (ArrayList<Map<String, String>>) dataSnapshot.getValue();
+                ArrayList<LatLng>latLngs =new ArrayList<>();
+                ArrayList<String>names = new ArrayList<>();
+                String name="";
+                for (Map<String, String> entry : locations) {
+                    for (String key : entry.keySet()) {
+                        String val = entry.get(key);
+                        if(key.equals("lat")){
+                             lat = Double.parseDouble(val);
+                        }
+                        else if(key.equals("lon")){
+                             lon = Double.parseDouble(val);
+                        }
+                        else if(key.equals("Name")){
+                            name = val;
+                        }
+                    }
+                    names.add(name);
+                    latLngs.add(new LatLng(lat,lon));
+                }
+                int i =0;
+                for(LatLng latLng:latLngs){
+                    googleMap.addMarker(new MarkerOptions().position(latLng).title(names.get(i)));
+                    i++;
+                }
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat,lon),18.0f));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+
+    @Override
     public void onLocationChanged(Location location) {
+
 
     }
 
