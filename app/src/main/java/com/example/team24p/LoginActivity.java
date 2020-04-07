3c26f0c4574fc8f7b3c645df7f180d49db915c0c
Gilad Cohen <gilad4591@gmail.com>
2020-04-08 02:52:38 +0300

    LOGIN + validation to password + profileActivity v2

--- app/src/main/java/com/example/team24p/LoginActivity.java
+++ app/src/main/java/com/example/team24p/LoginActivity.java
@@ -1,27 +1,101 @@
 package com.example.team24p;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buttonSignIn;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button buttonSignUp;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+        firebaseAuth = FirebaseAuth.getInstance();
+//        if (firebaseAuth.getCurrentUser()!= null){
+//            finish();
+//            startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+//        }
 
-        Button buttonRegister = (Button)findViewById(R.id.Register);
-        buttonRegister.setOnClickListener(new View.OnClickListener() {
+
+        editTextPassword = (EditText) findViewById(R.id.EditTextPassword);
+        editTextEmail =(EditText) findViewById(R.id.EditTextEmail);
+        buttonSignIn = (Button) findViewById(R.id.loginButton);
+        buttonSignUp = (Button) findViewById(R.id.signUpButton);
+
+        progressDialog = new ProgressDialog(this);
+
+        buttonSignUp.setOnClickListener(this);
+        buttonSignIn.setOnClickListener(this);
+    }
+
+    private void userLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        if (email.isEmpty()){
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please enter a valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if (password.isEmpty()){
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if (password.length() < 6){
+            editTextPassword.setError("Minimum length of password should be 6");
+            editTextPassword.requestFocus();
+            return;
+        }
+        progressDialog.setMessage("Login User, please wait");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
-            public void onClick(View v) {
-                Intent stRegister = new Intent(getApplicationContext(),SignUpActivity.class);
-                startActivity(stRegister);
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressDialog.dismiss();
+                if (task.isSuccessful()){
+                    finish();
+                    startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+
+                }
             }
         });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonSignIn){
+            userLogin();
+        }
+        if (view == buttonSignUp){
+            finish();
+            startActivity(new Intent(this,SignUpActivity.class));
+        }
     }
 
 
