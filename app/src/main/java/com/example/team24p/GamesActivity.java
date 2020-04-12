bba7c1b29c4ae80558f48f791290ab32dab4fac4
tamiryak <45327624+tamiryak@users.noreply.github.com>
2020-04-12 11:42:09 +0300

    calendar activity

--- app/src/main/java/com/example/team24p/GamesActivity.java
+++ app/src/main/java/com/example/team24p/GamesActivity.java
@@ -2,13 +2,52 @@ package com.example.team24p;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Locale;
 
 public class GamesActivity extends AppCompatActivity {
+    EditText selectDate,selectTime;
+    private int mYear, mMonth, mDay, mHour, mMinute;
+    private CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_games);
+        String markerName = getIntent().getStringExtra("markerName");
+
+        calendarView = (CalendarView)findViewById(R.id.cGames);
+        final Calendar currentCalendar = Calendar.getInstance(Locale.getDefault());
+        calendarView.setFirstDayOfWeek(Calendar.SUNDAY);
+        calendarView.setMinDate(System.currentTimeMillis()-1000);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month,
+                                            int dayOfMonth) {
+                Intent intent = new Intent(Intent.ACTION_EDIT);
+                intent.setType("vnd.android.cursor.item/event");
+                intent.putExtra("beginTime", currentCalendar.getTimeInMillis());
+                intent.putExtra("allDay", true);
+                intent.putExtra("rrule", "FREQ=YEARLY");
+                intent.putExtra("endTime", currentCalendar.getTimeInMillis()+60*60*1000);
+                intent.putExtra("title", "A Test Event from android app");
+                startActivity(intent);
+
+            }
+        });
     }
+
 }
