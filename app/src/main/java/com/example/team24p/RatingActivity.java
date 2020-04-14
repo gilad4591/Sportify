9a726f92465bc069acfde5aa7ba8677046681ebc
tamiryak <45327624+tamiryak@users.noreply.github.com>
2020-04-14 17:19:12 +0300

    rating works

--- app/src/main/java/com/example/team24p/RatingActivity.java
+++ app/src/main/java/com/example/team24p/RatingActivity.java
@@ -17,6 +17,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -31,7 +32,7 @@ public class RatingActivity extends AppCompatActivity {
     private RatingBar ratingBar;
     private int flag;
     private double sum=0.0;
-    private int count=1;
+    private int count=0;
 
 
 
@@ -42,68 +43,77 @@ public class RatingActivity extends AppCompatActivity {
         MarkerName = getIntent().getStringExtra("markerName");
         UserName =  getIntent().getStringExtra("userNameLoggedIn");
         ratingBar = findViewById(R.id.ratingBar);
+        final TextView rateUs = (TextView)findViewById(R.id.rateUs);
         flag = 0;
 
         mRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                ArrayList<Map<String, String>> ratings = (ArrayList<Map<String, String>>) dataSnapshot.getValue();
-                for (Map<String, String> entry : ratings) {
-                    for (String key : entry.keySet()) {
-                        String value = entry.get(key);
-                        if (key.equals("username") && (key.equals("ground"))) {
+                sum=0;
+                count=0;
+                Map<String, Object > ratings = (HashMap<String, Object>)  dataSnapshot.getValue();
+                    for (String key : ratings.keySet()) {
+                        Map<String, Object > value = (HashMap<String, Object>)ratings.get(key);
 
-                            if (value == UserName) flag = 1; // user already voted
-                        }
-                        if(key.equals("rating")){
+                            if (value.get("username").toString().equals(UserName) && value.get("ground").toString().equals(MarkerName)) flag = 1; // user already voted
+                        if(value.get("ground").toString().equals(MarkerName)) {
                             count++;
-                            sum+=Double.parseDouble(value);
+                            sum += Double.parseDouble(value.get("rating").toString());
                         }
                     }
+
+
+                if(flag==1){
+                    ratingBar.setVisibility(View.INVISIBLE);
+                    rateUs.setVisibility(View.INVISIBLE);
                 }
+
+                ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+                    @Override
+                    public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                        int r = (int) v;
+                        String message=null;
+                        if((UserName!=null) && (flag!=1)){
+                            switch(r){
+                                case 1: message="מתנצלים לשמוע!";
+                                    break;
+                                case 2: message = "נפעל ע''מ שיפור השירות!";
+                                    break;
+                                case 3: message = "תודה על תגובתך!";
+                                    break;
+                                case 4: message = "תודה לך!";
+                                    break;
+                                case 5: message = "יא מלך,מעריכים את זה!";
+                                    break;
+                            }
+                            Toast.makeText(RatingActivity.this, message , Toast.LENGTH_SHORT).show();
+
+                            Map<String, Object > rate = new HashMap<String, Object>();
+                            rate.put("ground", MarkerName);
+                            rate.put("rating", String.valueOf(v));
+                            rate.put("username", UserName);
+                            if(String.valueOf(v)!="0") mRef.push().updateChildren(rate);
+                        }
+                        else if(flag==1)Toast.makeText(RatingActivity.this, "You already voted for this ground" , Toast.LENGTH_SHORT).show();
+                        else Toast.makeText(RatingActivity.this, "You are not logged in. \n log in before rate" , Toast.LENGTH_SHORT).show();
+                    }
+                });
+                double avg = sum/count;
+
+                TextView avgTextView = (TextView)findViewById(R.id.avgRating);
+
+                DecimalFormat ff = new DecimalFormat("#0.00");
+                avgTextView.setText(ff.format(avg)+"/"+5);
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
+
         });
 
-        if(flag==1) ratingBar.setVisibility(View.INVISIBLE);
 
-        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-            @Override
-            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
-                int r = (int) v;
-                String message=null;
-                if(UserName!=null) {
-                    switch(r){
-                        case 1: message="מתנצלים לשמוע!";
-                            break;
-                        case 2: message = "נפעל ע''מ שיפור השירות!";
-                            break;
-                        case 3: message = "תודה על תגובתך!";
-                            break;
-                        case 4: message = "תודה לך!";
-                            break;
-                        case 5: message = "יא מלך,מעריכים את זה!";
-                            break;
-                    }
-                    Toast.makeText(RatingActivity.this, message , Toast.LENGTH_SHORT).show();
-
-                    Map<String, Object > rate = new HashMap<String, Object>();
-                    rate.put("ground", MarkerName);
-                    rate.put("rating", String.valueOf(v));
-                    rate.put("username", UserName);
-                    if(String.valueOf(v)!="0") mRef.push().updateChildren(rate);
-                }
-                else Toast.makeText(RatingActivity.this, "You are not logged in. \n log in before rate" , Toast.LENGTH_SHORT).show();
-            }
-        });
-        double avg = sum/count;
-        TextView avgTextView = (TextView)findViewById(R.id.avgRating);
-
-        avgTextView.setText(String.valueOf(avg)+"/"+5);
 
     }
 
