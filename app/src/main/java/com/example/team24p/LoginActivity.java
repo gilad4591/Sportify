f666b2e6c193c7f7641e6bf935d5a3eb27104bcb
Gilad Cohen <gilad4591@gmail.com>
2020-04-15 04:58:22 +0300

    login bug fix(change from arraylist to hashmap because of casting problems)

--- app/src/main/java/com/example/team24p/LoginActivity.java
+++ app/src/main/java/com/example/team24p/LoginActivity.java
@@ -20,6 +20,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
@@ -76,37 +77,23 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         mRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                ArrayList<UsersAndPasswords> userAndPassArrayList =new ArrayList<>();
-                ArrayList<Map<String, String>> usersList = (ArrayList<Map<String, String>>) dataSnapshot.getValue();
-                for (Map<String, String> entry : usersList) {
-                    UsersAndPasswords userAndPass = new UsersAndPasswords();
-                    for (String key : entry.keySet()) {
-                        String value = String.valueOf(entry.get(key));
-                        System.out.println(key + ":" + value);
-                        switch (key) {
-                            case "UserName":
-                                userAndPass.setUserName(value);
-                                break;
-                            case "Password":
-                                userAndPass.setPassword(value);
-                                break;
-                        }
+                int succeded = 0;
+                Map<String, Object> usersList = (HashMap<String, Object>) dataSnapshot.getValue();
+                    for (String key : usersList.keySet()) {
+                        Map<String,Object> value = (HashMap<String,Object>)usersList.get(key);
+                        if (email.equalsIgnoreCase(value.get("UserName").toString()) && password.equals(value.get("Password").toString())) {
+                            Toast.makeText(getApplicationContext(), "User login successfully", Toast.LENGTH_SHORT).show();
+                            finish();
+                            succeded = 1;
+                            Intent intent = new Intent(LoginActivity.this , MainActivity.class);
+                            intent.putExtra("userNameLoggedIn", email);
+                            MainActivity.count+=1;
+                            startActivity(intent);
 
                     }
-                    userAndPassArrayList.add(userAndPass);
-                }
+                    //userAndPassArrayList.add(userAndPass);
+                //}
                 progressDialog.dismiss();
-                int succeded = 0;
-                for(UsersAndPasswords us : userAndPassArrayList){
-                    if (email.equalsIgnoreCase(us.getUserName()) && password.equals(us.getPassword())) {
-                        Toast.makeText(getApplicationContext(), "User login successfully", Toast.LENGTH_SHORT).show();
-                        finish();
-                        succeded = 1;
-                        Intent intent = new Intent(LoginActivity.this , MainActivity.class);
-                        intent.putExtra("userNameLoggedIn", email);
-                        MainActivity.count+=1;
-                        startActivity(intent);
-                    }
                 }
                 if (succeded == 0) {
                     Toast.makeText(getApplicationContext(), "User or password incorrect", Toast.LENGTH_SHORT).show();
