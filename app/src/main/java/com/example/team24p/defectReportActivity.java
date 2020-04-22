c87e9f06563c0434caca1b028095270c93480988
afik <afikziv@gmail.com>
2020-04-22 21:05:03 +0300

    defact activity to DB

--- app/src/main/java/com/example/team24p/defectReportActivity.java
+++ app/src/main/java/com/example/team24p/defectReportActivity.java
@@ -1,11 +1,18 @@
 package com.example.team24p;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class defectReportActivity extends AppCompatActivity {
 
@@ -16,5 +23,16 @@ public class defectReportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_defect_report);
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Map<String, Object > Defacts = (HashMap<String, Object>)  dataSnapshot.getValue();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
