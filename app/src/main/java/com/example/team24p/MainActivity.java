13057b0c6c571ea65100d4870507c37b8646bd7e
tamiryak <45327624+tamiryak@users.noreply.github.com>
2020-05-16 16:46:50 +0300

    styling

--- app/src/main/java/com/example/team24p/MainActivity.java
+++ app/src/main/java/com/example/team24p/MainActivity.java
@@ -3,15 +3,23 @@ package com.example.team24p;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -41,23 +49,49 @@ public class MainActivity extends AppCompatActivity {
     private DatabaseReference mRef = mDatabase.getReference().child("Events");
     private FloatingActionButton adminPanelButton;
     private FloatingActionButton messageBut;
+    private FloatingActionButton helpBut;
     private String userNameLoggedIn = "";
     private ArrayList<String> items;
+    private View help;
     private ListView myAct;
+    private TextView firstText;
+    private TextView mesText;
+    private TextView logoutText;
+    private TextView loginText;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         items = new ArrayList<>();
         items.clear();
         messageBut = (FloatingActionButton) findViewById(R.id.messageButton);
+        firstText = (TextView)findViewById(R.id.firstText);
         messageBut.setVisibility(View.INVISIBLE);
         welcomeTextView = (TextView) findViewById(R.id.welcomeTextView);
         editPrivateText = (TextView)findViewById(R.id.editPrivateText);
+        mesText = (TextView)findViewById(R.id.mestext);
+        logoutText = (TextView)findViewById(R.id.logouttext);
+        loginText = (TextView)findViewById(R.id.logtext);
         editPrivateText.setFocusable(false);
         editPrivateText.setClickable(true);
         editPrivateText.setVisibility(View.INVISIBLE);
+        firstText.setVisibility(View.VISIBLE);
+        mesText.setVisibility(View.INVISIBLE);
+        logoutText.setVisibility(View.INVISIBLE);
+        loginText.setVisibility(View.VISIBLE);
+        helpBut = (FloatingActionButton)findViewById(R.id.helpBut);
+        help = (View)findViewById(R.id.helpView);
+
+        helpBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonShowPopupWindowClick(help);
+            }
+        });
+
+
         adminPanelButton = (FloatingActionButton) findViewById(R.id.buttonManagePanel);
         FloatingActionButton logoutButton = (FloatingActionButton) findViewById(R.id.logoutButton);
         adminPanelButton.setVisibility(View.INVISIBLE);
@@ -93,7 +127,11 @@ public class MainActivity extends AppCompatActivity {
                 count++;
             }
             if (userNameLoggedIn != null){
+                firstText.setVisibility(View.INVISIBLE);
                 LoginButton.setVisibility(View.INVISIBLE);
+                loginText.setVisibility(View.INVISIBLE);
+                mesText.setVisibility(View.VISIBLE);
+                logoutText.setVisibility(View.VISIBLE);
                 logoutButton.setVisibility(View.VISIBLE);
                 myAct.setVisibility(View.VISIBLE);
                 welcomeTextView.setText("Welcome"+" " +(userNameLoggedIn));
@@ -206,6 +244,34 @@ public class MainActivity extends AppCompatActivity {
                 Intent stMess = new Intent(MainActivity.this,messageActivity.class);
                 stMess.putExtra("userNameLoggedIn",userNameLoggedIn);
                 startActivity(stMess);
+            }
+        });
+
+    }
+    public void onButtonShowPopupWindowClick(View view) {
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.
+                popup_window, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        FloatingActionButton cb = popupView.findViewById(R.id.closeBut);
+        cb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
             }
         });
 
