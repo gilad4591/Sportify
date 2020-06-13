6a88a85b5fc9418a5c139e8198640fd9c33877f8
Gilad Cohen <gilad4591@gmail.com>
2020-06-13 17:14:16 +0300

    comment userAdmin activity

--- app/src/main/java/com/example/team24p/userAdminActivity.java
+++ app/src/main/java/com/example/team24p/userAdminActivity.java
@@ -20,17 +20,17 @@ public class userAdminActivity extends AppCompatActivity {
 
     TextView userToEditTextView;
     private FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
-    private DatabaseReference refToDelete = mDatabase.getReference().child("UsersAndPasswords");
-    private DatabaseReference refChangeToDisable;
+    private DatabaseReference refToDelete = mDatabase.getReference().child("UsersAndPasswords"); //connect to relevant table from db
+    private DatabaseReference refChangeToDisable; // refernce if need to delete user
     Button deleteUserButton;
     Button editAccessButton;
     Button editUserButton;
     String userNameLoggedIn="";
     String userToEdit = "";
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //--------init and link buttons
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_admin);
         userNameLoggedIn = getIntent().getStringExtra("userNameLoggedIn");
@@ -49,7 +49,7 @@ public class userAdminActivity extends AppCompatActivity {
                 refToDelete.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot)
-                    {
+                    { //---move strings to hashmap so we can user them
                         String keyToChange = "";
                         Map<String, Object> usersList = (HashMap<String, Object>) dataSnapshot.getValue();
                         for (String key : usersList.keySet()) {
@@ -62,14 +62,18 @@ public class userAdminActivity extends AppCompatActivity {
                                     userData.put("UserName", userToEdit);
                                     userData.put("isAdmin", value.get("isAdmin").toString());
                                     userData.put("enabled", "False");
+                                    // if user deleted it will change the "enabled" part to false, then the user wont be able to connect.
+                                    // if we want to delete it permnantely we need to delete from db, its just safe delete.
                                     refChangeToDisable = refToDelete.child(key);
                                     refChangeToDisable.setValue(userData);
                                     Toast.makeText(getApplicationContext(), "User deleted successfully", Toast.LENGTH_SHORT).show();
                                 }
                                 else {
                                     Toast.makeText(getApplicationContext(), "User already deleted", Toast.LENGTH_SHORT).show();
+                                    //if we try to delete already deleted user
                                 }
                                 Intent stDeleteUser = new Intent(getApplicationContext(),MainActivity.class);
+                                // pass args to other activity
                                 stDeleteUser.putExtra("userNameLoggedIn",userNameLoggedIn);
                                 stDeleteUser.putExtra("isAdmin",getIntent().getStringExtra("isAdmin"));
                                 startActivity(stDeleteUser);
@@ -90,6 +94,7 @@ public class userAdminActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent stEditUser = new Intent(getApplicationContext(),EditUserDetailsActivity.class);
+                // pass args to other activity
                 stEditUser.putExtra("userNameLoggedIn",userNameLoggedIn);
                 stEditUser.putExtra("userToEdit",userToEdit);
                 stEditUser.putExtra("isAdmin",getIntent().getStringExtra("isAdmin"));
@@ -103,6 +108,7 @@ public class userAdminActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent stEditUserAccess = new Intent(getApplicationContext(),editUserAccessActivity.class);
+                // pass args to other activity
                 stEditUserAccess.putExtra("userNameLoggedIn",userNameLoggedIn);
                 stEditUserAccess.putExtra("userToEdit",userToEdit);
                 stEditUserAccess.putExtra("isAdmin",getIntent().getStringExtra("isAdmin"));
