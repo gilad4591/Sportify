61a919aabcfe759a351b1d030d89839ea94bca1f
Gilad Cohen <gilad4591@gmail.com>
2020-06-14 16:12:06 +0300

    for jenkins

--- app/src/test/java/com/example/team24p/ExampleUnitTest.java
+++ app/src/test/java/com/example/team24p/ExampleUnitTest.java
@@ -15,6 +15,7 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
 public class ExampleUnitTest {
     UsefullFunctions us = new UsefullFunctions();
 
